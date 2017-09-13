5a154da94935b78ff5cf0a90fbd1c45b83aa0254
Shinsuke Sugaya <shinsuke@apache.org>
2017-09-13 15:34:22 +0900

    update project info

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__author__ = "The PredictionIO Team"
-__email__ = "help@tappingstone.com"
+__author__ = "Apache PredictionIO"
+__email__ = "user@predictionio.incubator.apache.org"
 __copyright__ = "Copyright 2014, TappingStone, Inc."
 __license__ = "Apache License, Version 2.0"
 
@@ -14,7 +14,7 @@ setup(
     author=__author__,
     author_email=__email__,
     packages=['predictionio'],
-    url='http://prediction.io',
+    url='http://predictionio.incubator.apache.org/',
     license='LICENSE.txt',
     description='PredictionIO Python SDK',
     classifiers=[
@@ -44,7 +44,7 @@ setup(
                        - without worrying about scalability
 
                        Detailed documentation is available on our
-                       `documentation site <http://docs.prediction.io>`_.
+                       `documentation site <http://predictionio.incubator.apache.org/>`_.
 
                        This module provides convenient access of the
                        PredictionIO API to Python programmers so that they
